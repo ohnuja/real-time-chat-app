da6c89f1c3088def532f2637211e0c4281595a2e
Anuja <anujadeb04@gmail.com>
2026-01-16 11:45:27 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def handle_room_message(data):
 if __name__ == '__main__':
     socketio.run(app, host='127.0.0.1', port=8000, debug=True)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
 
