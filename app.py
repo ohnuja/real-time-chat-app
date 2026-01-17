bd6ca66fcac89afdee50e53f69855f5d9a6a6a14
Anuja <anujadeb04@gmail.com>
2026-01-17 14:44:55 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -7,29 +7,35 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
+# Upload config
 UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Track users per room
-users_in_rooms = {}
+# In-memory storage
+users_in_rooms = {}      # { room: [user1, user2] }
+user_sessions = {}       # { sid: {username, room} }
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# ---------------- SOCKET EVENTS ---------------- #
 
 @socketio.on('join_room')
 def handle_join(data):
     username = data['username']
     room = data['room']
+
+    user_sessions[request.sid] = {
+        "username": username,
+        "room": room
+    }
 
     join_room(room)
 
@@ -42,21 +48,17 @@ def handle_join(data):
     emit('update_users', users_in_rooms[room], to=room)
     emit('message', f"{username} joined the room", to=room)
 
-
 @socketio.on('room_message')
 def handle_message(data):
     emit('message', data['msg'], to=data['room'])
-
 
 @socketio.on('typing')
 def handle_typing(data):
     emit('show_typing', data['username'], to=data['room'], include_self=False)
 
-
 @socketio.on('stop_typing')
 def handle_stop_typing(data):
     emit('hide_typing', data['username'], to=data['room'], include_self=False)
-
 
 @socketio.on('image_upload')
 def handle_image(data):
@@ -66,7 +68,7 @@ def handle_image(data):
         return
 
     image_data = base64.b64decode(data['file'].split(',')[1])
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
 
     with open(filepath, 'wb') as f:
         f.write(image_data)
@@ -76,13 +78,26 @@ def handle_image(data):
         'image_url': f"/static/uploads/{filename}"
     }, to=data['room'])
 
-
 @socketio.on('disconnect')
 def handle_disconnect():
-    for room in users_in_rooms:
-        emit('update_users', users_in_rooms[room], to=room)
+    user = user_sessions.get(request.sid)
+    if not user:
+        return
 
+    room = user['room']
+    username = user['username']
+
+    if room in users_in_rooms and username in users_in_rooms[room]:
+        users_in_rooms[room].remove(username)
+
+        emit('update_users', users_in_rooms[room], to=room)
+        emit('message', f"{username} left the room", to=room)
+
+    del user_sessions[request.sid]
+
+# ---------------- RUN ---------------- #
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
+
